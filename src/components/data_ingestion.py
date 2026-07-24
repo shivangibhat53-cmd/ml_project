b11969879779737e4ebd9533d1ae776f3ed10339
Shiv <shivangibhat93@gmail.com>
2026-07-24 11:03:29 -0700

    Data Transformation pipeline created

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,12 +6,12 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-
+from src.components.data_transformation import DataTransformation
 @dataclass
 class DataIngestionConfig:
-    train_data_path : str = os.path.join('artificats','train.csv')
-    test_data_path  : str = os.path.join('artificats','test.csv')
-    raw_data_path   : str = os.path.join('artificats','data.csv')
+    train_data_path : str = os.path.join('artifacts','train.csv')
+    test_data_path  : str = os.path.join('artifacts','test.csv')
+    raw_data_path   : str = os.path.join('artifacts','data.csv')
 
 class DataIngestion:
     def __init__(self):
@@ -40,6 +40,4 @@ class DataIngestion:
         except Exception as e:
             raise CustomException(e,sys)
 
-if __name__ == "__main__":    
-    obj = DataIngestion()     
-    obj.intiate_data_ingestion()
+
